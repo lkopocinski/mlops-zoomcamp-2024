62ddd751c0ee4cdae1fd295b995b12934e4457a7
Łukasz Kopociński <lkopocinski@gmail.com>
2024-05-25 21:44:49 +0200

    Run local tracking serwer

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -2,9 +2,10 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 
 def load_pickle(filename: str):
@@ -19,6 +20,8 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.autolog()
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
@@ -27,7 +30,7 @@ def run_train(data_path: str):
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    rmse = root_mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
